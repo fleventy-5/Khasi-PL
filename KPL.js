75e77fd378f2a25bb8bc70d17c7186ba1b074bab
fleventy-5 <templyngkhoi@gmail.com>
2021-03-07 16:28:54 +0530

    Tokenizer test

--- KPL.js
+++ KPL.js
@@ -1,4 +1,4 @@
-let keywords = 'shon pan lada hynrei badlada lehhaba haba naduh haduh dak kyntien num';
+let keywords = "shon pan lada hynrei badlada lehhaba haba naduh haduh dak kyntien num";
 
 
 function Token(input){
@@ -6,24 +6,24 @@ function Token(input){
 	stringTok = input.split(" ");
 	token = []
 	stringTok.forEach(function(stringTok){
-		if(keywords.indexOf(" "+ stringTok + " ") >=0){
+		if(keywords.includes(stringTok) && stringTok !=" "){
 			token.push({
-				type: "keywords",
+				type: "keyword",
 				value : stringTok
 			})
 		} else if(/[0-9]/i.test(stringTok)){
 			token.push({
-				type: "Number",
+				type: 'Number',
 				value : stringTok
 			})
 		} else if(/[a-z]/i.test(stringTok) || /[A-Z]/i.test(stringTok)){
 			token.push({
-				type: "Character",
+				type: "char",
 				value : stringTok
 			})
 		} 
 	})
 	return token;
 }
-let a = JSON.stringify(Token(" num A ,num B + 245"))
+let a = JSON.stringify(Token("num A , num B + 245"))
 document.write(a);
